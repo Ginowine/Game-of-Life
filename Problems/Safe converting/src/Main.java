e683c21903be30deb285592ffabf69e500dd1977
ginowine <ginowine1@gmail.com>
2020-12-01 17:26:09 +0100

    NPE

---
+++ Problems/Safe converting/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int convert(Long val) {
+        // write your code here
+//        int value = val.intValue();
+        if (val == null){
+            return 0;
+        }else if (val > Integer.MAX_VALUE){
+            int a = (int)2147483647;
+            return a;
+        }else if (val < Integer.MIN_VALUE){
+            int b = (int)-2147483648;
+            return b;
+        }else {
+            return val.intValue();
+        }
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String val = scanner.nextLine();
+        Long longVal = "null".equals(val) ? null : Long.parseLong(val);
+        System.out.println(convert(longVal));
+    }
+}
