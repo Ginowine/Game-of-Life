c0b3de552ef75252e43d1ac76c379ade19ffe16d
ginowine <ginowine1@gmail.com>
2020-12-13 20:00:07 +0100

    stage 3

--- Game of Life/task/src/life/GenerationAlgorithm.java
+++ Game of Life/task/src/life/GenerationAlgorithm.java
@@ -1,39 +1,15 @@
 package life;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class GenerationAlgorithm {
-    private String[][] field;
-
-    public GenerationAlgorithm(String[][] field) {
-        this.field = field;
-    }
-
-    public String[][] calculateNextGeneration() {
-        String[][] oldField = Arrays.stream(this.field).map(String[]::clone).toArray(String[][]::new);
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field.length; j++) {
-                int countOfNeighbors = countNeighbors(oldField, i, j);
-                if (oldField[i][j] == " " && countOfNeighbors == 3) {
-                    this.field[i][j] = "O";
-                } else if (oldField[i][j] == "O" && (countOfNeighbors < 2 || countOfNeighbors > 3)) {
-                    this.field[i][j] = " ";
-                }
+    static Universe getNextGeneration(Universe currentUniverse) {
+        Universe nextGeneration = new Universe(currentUniverse.size());
+        for (int row = 0; row < currentUniverse.size(); ++row) {
+            for (int col = 0; col < currentUniverse.size(); ++col) {
+                final int neighbours = currentUniverse.getNeighboursCount(row, col);
+                final var isLive = neighbours == 3 || neighbours == 2 && currentUniverse.isLive(row, col);
+                nextGeneration.setCell(row, col, isLive);
             }
         }
-        return this.field;
-    }
-
-    private int countNeighbors(String[][] oldField, int row, int column) {
-        int neighbors = 0;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (oldField[(field.length+row+i)%field.length][(field.length+column+j)%field.length] == "O" && (i!=0 || j!=0)) {
-                    neighbors++;
-                }
-            }
-        }
-        return neighbors;
+        return nextGeneration;
     }
 }
--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,29 +1,54 @@
 package life;
 
-import javafx.scene.control.Cell;
-
-import java.util.Random;
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        final int size = sc.nextInt();
-        final int seed = sc.nextInt();
-        final int numberOfGenerations = sc.nextInt();
-        sc.close();
-
-        Universe universe = new Universe(size, seed);
-        String[][] field = universe.evolve();
-
-
-        GenerationAlgorithm algo = new GenerationAlgorithm(field);
-        for (int i = 0; i < numberOfGenerations; i++) {
-            field = algo.calculateNextGeneration();
+    public static void main(String[] args) throws InterruptedException {
+//        Scanner sc = new Scanner(System.in);
+//        final int size = sc.nextInt();
+//        final int seed = sc.nextInt();
+//        final int numberOfGenerations = sc.nextInt();
+//        sc.close();
+//
+//        Universe universe = new Universe(size, seed);
+//        String[][] field = universe.evolve();
+//
+//
+//        GenerationAlgorithm algo = new GenerationAlgorithm(field);
+//        for (int i = 0; i < numberOfGenerations; i++) {
+//            field = algo.calculateNextGeneration();
+//        }
+//
+//        universe.printUniverse(field, size);
+        Scanner scanner = new Scanner(System.in);
+        int universeSize = scanner.nextInt();
+        final int generations = 11;
+        scanner.close();
+        Universe universe = new Universe(universeSize).initialise();
+        for (int i = 0; i < generations; ++i) {
+            System.out.printf("Generation #%d%nAlive: %d%n", universe.getGeneration(),
+                    universe.getAliveCount());
+            universe.printMap();
+            universe.nextGeneration();
+            Thread.sleep(1000);
+            clearScreen();
         }
-
-        universe.printUniverse(field, size);
+    }
+    static void clearScreenOld() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
